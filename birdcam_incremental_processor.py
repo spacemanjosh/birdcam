3c574ad71327d021fdd28b5ab20cbf92569ad30c
Josh Elliott <joshua.peter.elliott@gmail.com>
2025-05-16 16:00:02 -0700

    Fixed a typo

--- birdcam_incremental_processor.py
+++ birdcam_incremental_processor.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dropbox_dir = args.input_path
-    staging_dir = args.ouptut_path
+    staging_dir = args.output_path
     archive_dir = args.archive_path
 
     processor = BirdcamProcessor(dropbox_dir, staging_dir, archive_dir)
